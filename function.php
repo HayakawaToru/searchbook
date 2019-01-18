54b19627e0da6f56603433c5e4450a3872c27ef9
Tarozaemon <headline10th@gmail.com>
2019-01-18 16:37:07 +0900

    add dbConnect

--- function.php
+++ function.php
@@ -1,4 +1,20 @@
 <?php
+function dbConnect(){
+    $dsn = 'mysql:dbname=search_book;host=localhost:8889;charset=utf8';
+    $user = 'root';
+    $password ='root';
+    $options = array(
+      // SQL実行失敗時にはエラーコードのみ設定
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
+      // デフォルトフェッチモードを連想配列形式に設定
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      // バッファードクエリを使う(一度に結果セットをすべて取得し、サーバー負荷を軽減)
+      // SELECTで得た結果に対してもrowCountメソッドを使えるようにする
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    );
+    $dbh = new PDO($dsn, $user, $password, $options);
 
+    return $dbh;
+}
 
 ?>
